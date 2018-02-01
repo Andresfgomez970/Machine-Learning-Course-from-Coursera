81c1366991689e6945fc1b940b16cce75f0ec101
Andresfgomez970 <Andresf.gomez970@gmail.com>
2018-02-01 03:17:26 -0500

    Update displayData.m

--- week3/ex3/displayData.m
+++ week3/ex3/displayData.m
@@ -11,7 +11,9 @@ function [h, display_array] = displayData(X, example_width)
 %   isempty return True if the input is a 0 matrix
 
 if ~exist('example_width', 'var') || isempty(example_width)
-	%This suppose that the image is an square  
+	%This suppose that the image is an square
+	%  take care if your image is not because you would
+	%  get then an error.
 	example_width = round(sqrt(size(X, 2)));
 end
 
@@ -20,11 +22,18 @@ colormap(gray);
 
 % Compute rows, cols
 [m n] = size(X);
-%As n is a number that could represent the are of the image
-% iun this way we can get the height 
+%As n is a number that could represent the area of the image
+% in some arbitary units, the height is obtained as follow in 
+% that units too  
 example_height = (n / example_width); 
 
 % Compute number of items to display
+% In general this is to show mxm element
+% or mx(m+1) because ceil(m / display_rows)
+% is like to have ceil(m / floor( sqrt(m)) )
+% and so the numerator is bigger or equal to the 
+% denominator and the result is therefore 
+% sqrt(m)+1 or sqrt(m).
 display_rows = floor(sqrt(m));
 display_cols = ceil(m / display_rows);
 
@@ -47,7 +56,10 @@ for j = 1:display_rows
 		% Copy the patch
 		
 		% Get the max value of the patch to divide by this one later 
-		%  making this one to represent the most dark one,
+		%  making this one to represent the most dark one in a given 
+		%  image; in some way this set the values of each image
+		%  in a same scale; here we are telling to the program where to 
+		%  put our example images in each row of X.
 		max_val = max(abs(X(curr_ex, :))); 
 		display_array(pad + (j - 1) * (example_height + pad) + (1:example_height), ...
 		             pad + (i - 1) * (example_width + pad) + (1:example_width)) = ...
